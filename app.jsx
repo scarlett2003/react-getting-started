154f5488757fd04b986777cd54b5cc22ba7a3d55
vialley-imac <jovilee09@gmail.com>
2016-01-26 00:13:31 +0800

    react組件的事件和表單處理

--- app.jsx
+++ app.jsx
@@ -14,11 +14,29 @@ class Comment extends React.Component {
 }
 
 class CommentForm extends React.Component {
+	
+	handleSubmit(e) {
+		e.preventDefault();
+		// console.log(this, e);
+		// const是定義常量 es2015的寫法，同 var
+		// refs react內置的一個對像，獲取author的值， getDOMNode方法獲取對應html的對像， .value 獲取到值， trim() 是javascript的方法，刪除前後空格
+		const author = this.refs.author.getDOMNode().value.trim();
+		const body = this.refs.body.getDOMNode().value.trim();
+		const form = this.refs.form.getDOMNode();
+		// console.log(author,body);
+		
+		this.props.onSubmit({author: author, body: body});
+
+		form.reset();
+	}
+
 	render() {
 		return(
-			<div className="comment-form">
-				CommentForm
-			</div>
+			<form className="comment-form" ref="form" onSubmit={(e) => {this.handleSubmit(e)}}>
+				<input type="text" placeholder="Your name" ref="author" />
+				<input type="text" placeholder="Input your comment" ref="body" />
+				<input type="submit" value="Add Comment" />
+			</form>
 		);
 	}
 }
@@ -64,13 +82,39 @@ class CommentBox extends React.Component {
 	componentDidMount() {
 		this.loadDataFromServer();
 	}
+	
+	handleNewComment(comment) {
+		// console.log(comment);
+
+		const comments = this.state.comments;
+		const newComments = comments.concat([comment]);
+		this.setState({comments: newComments});
+		
+		// 為了測試失敗後，變更回原本state的值，所以設定了一個 setTimeout的方法來延遲2秒
+		setTimeout(() => {
+			$.ajax({
+				url: this.props.url,
+				dataType: "json",
+				type: "POST",
+				data: comment,
+				success: (comments) => {
+					this.setState({comments: comments});
+				},
+				error: (xhr, status, err) => {
+					console.log(err.toString());
+					this.setState({comments: comments});
+				}
+			});			
+		}, 2000);
+
+	}
 
 	render() {
 		return (
 			<div className="comment-box">
 				<h1>Comments</h1>
 				<CommentList comments={this.state.comments} />
-				<CommentForm />
+				<CommentForm onSubmit={comment => {this.handleNewComment(comment)}} />
 			</div>
 		);
 	}
