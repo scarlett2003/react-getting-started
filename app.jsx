5282cb701ea57cdfba9fc64d07de587bb871477d
vialley-imac <jovilee09@gmail.com>
2016-01-25 14:18:12 +0800

    react中的屬性

--- app.jsx
+++ app.jsx
@@ -1,3 +1,18 @@
+class Comment extends React.Component {
+	render() {
+		return(
+			<div>
+				<div className="comment-body">
+					{this.props.children}
+				</div>
+				<div className="comment-author">
+					- {this.props.author}
+				</div>
+			</div>
+		);
+	}
+}
+
 class CommentForm extends React.Component {
 	render() {
 		return(
@@ -12,7 +27,15 @@ class CommentList extends React.Component {
 	render() {
 		return(
 			<div className="comment-list">
-				CommentList
+				<Comment author="jovi">
+					This is my comment.
+				</Comment>
+				<Comment author="leon">
+					This is my comment2.
+				</Comment>
+				<Comment author="jenny">
+					This is my comment3.
+				</Comment>
 			</div>
 		);
 	}
